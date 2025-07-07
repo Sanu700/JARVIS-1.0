9355329835d5badedc4f98db033fb3b83fb6ebef
Sanu700 <156925758+Sanu700@users.noreply.github.com>
2025-07-07 10:29:55 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# todo: Add your api key here
-apikey = "AIzaSyA4fNVPLPnG0KpwV9rRuneCKJbe9C4QZ1A"
-news_api_key = "9f1673c3ad244b07b119ba55644fee5e"
-weather_api_key = "49914c848bad492ea69195033251403"
-spotify_client_id = "321f9b524e82498c81330afeb178d491"
-spotify_client_secret = "4d997299e52543149ca92c9f19b27b72"
-spotify_redirect_uri = "http://localhost:8888/callback"
